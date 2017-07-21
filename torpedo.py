4a7c33dc607a6896b2f18f16c9d07b1c244f1faa
Kyle Verheyen <verheykr@rose-hulman.edu>
2017-07-21 14:08:36 -0400

    Torpedo animation now works, torpedo points and moves in the specified direction.

--- torpedo.py
+++ torpedo.py
@@ -9,30 +9,48 @@ class Torpedo(pygame.sprite.Sprite):
         self.screen = screen
         self.dx = dx
         self.dy = dy
-        self.image = pygame.image.load("torpedo.png")
-        self.image = pygame.transform.scale(self.image, (60, 60))
-        try:
-            self.image = pygame.transform.rotate(self.image, math.degrees(math.atan(self.dx/self.dy)))
-        except ZeroDivisionError:
-            pass
-        
+        self.imagePos = 0
+        self.imageList = ["torpedo1.png", "torpedo2.png", "torpedo3.png"]
+        self.image = pygame.image.load(self.imageList[0])
+
         self.originalRect = self.image.get_rect()
         self.originalRect.center = (500, 700)
         self.rect = self.originalRect
         self.count = 0
 
     def draw(self):
-        self.screen.blit(self.image, (self.rect.x, self.rect.y))
 
+                
+        
+        print("Image pos: " + str(self.imagePos))
+        self.image = pygame.image.load(self.imageList[self.imagePos])
+        self.image = pygame.transform.scale(self.image, (50, 50))
+
+        self.originalRect = self.image.get_rect()
+        self.originalRect.center = (500, 700)
+        
+        
+        try:
+            self.image = pygame.transform.rotate(self.image, math.degrees(math.atan(self.dx/self.dy)))
+        except ZeroDivisionError:
+            pass
+
+        
+
+        self.screen.blit(self.image, (self.rect.x, self.rect.y))
+        
+        
+
+        print(self.rect.x, self.rect.y)
     def move(self):
         
-        #delay = 0.6
-        #oldTime = time.time()
-        #if oldTime + delay > time.time():
-        #time.sleep(0.6)
+        self.rect.x = 500
+        self.rect.y = 700
         self.rect = self.originalRect.move(self.dx/200.0 * self.count, self.dy/200.0 * self.count)
         self.draw()
         self.count += 1
-       
+        self.imagePos +=1
+        if self.imagePos == 3:
+            self.imagePos = 0
         
-
+       
