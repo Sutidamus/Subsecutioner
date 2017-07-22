14d9a4a0026e70d55dec23313ac49980d1473692
Stephen Payne <paynesc@rose-hulman.edu>
2017-07-22 00:02:32 -0400

    Torpedo shrinks as it moves to surface level, torpedo sprite changes at a slower rate

--- gameClass.py
+++ gameClass.py
@@ -1,4 +1,3 @@
-
 import pygame
 import background
 import sys
--- torpedo.py
+++ torpedo.py
@@ -17,12 +17,14 @@ class Torpedo(pygame.sprite.Sprite):
         self.originalRect.center = (500, 700)
         self.rect = self.originalRect
         self.count = 0
+        self.scaleX = 100
+        self.scaleY = 100
     def __del__(self):
         print ("Torpedo Destroyed")
     def draw(self):  
         print("Image pos: " + str(self.imagePos))
         self.image = pygame.image.load(self.imageList[self.imagePos])
-        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.image = pygame.transform.scale(self.image, (self.scaleX, self.scaleY))
 
         self.originalRect = self.image.get_rect()
         self.originalRect.center = (500, 700)
@@ -46,7 +48,15 @@ class Torpedo(pygame.sprite.Sprite):
         self.rect.y = 700
         self.rect = self.originalRect.move(self.dx/200.0 * self.count, self.dy/200.0 * self.count)
         self.count += 1
-        self.imagePos +=1
+        if pygame.time.get_ticks() % 12 == 0:
+            self.imagePos +=1
+        if pygame.time.get_ticks() % 2 == 0:
+            if self.scaleX == 1 or self.scaleY == 1:
+                self.scaleX = 1
+                self.scaleY = 1
+            else:
+                self.scaleX -= 1
+                self.scaleY -= 1
         if self.imagePos == 3:
             self.imagePos = 0
         self.draw()
